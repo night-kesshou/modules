8fa902c3c56f3975c5d881576b10591b77cd1854
ZoneTwelve <zonetwelve159@gmail.com>
2018-12-29 01:22:18 +0800

    登入模組

---
+++ login.captcha.js
@@ -0,0 +1,40 @@
+const request = require("request").defaults({encoding:null});
+const cookie = require('cookie');
+const urls = {
+  main:"http://210.70.131.56/online/information.asp",
+  captcha:"http://210.70.131.56/online/image/vcode.asp?vcode=0",
+}
+
+function setup(returnIt){
+  var jar = request.jar();
+  let options = {
+    url:urls.main,
+    jar:jar
+  }
+  request(options, (e,r,d)=> returnIt((r&&r.statusCode===200?{jar:jar}:{error:e})) );
+}
+
+function main(callback, urlcode = false){
+  setup(({error, jar})=>{
+    if(!error&&jar){
+      let options = {
+        url:urls.captcha,
+        jar:jar
+      }
+      request(options, (e,r,d)=>{
+        if(e||!d||r.statusCode!==200)
+          return callback({error:"Get CAPTCHA error"});
+        let captcha = new Buffer(d).toString("base64");
+        let data = `data:image/Gif;base64,${captcha}`;
+        //let cookie = jar['_jar']['store']['idx']['210.70.131.56']['/'];
+        //let key = Object.keys(cookie).pop();
+        //  data = `data:${r.headers["content-type"]}base64,${captcha}`;
+        return callback({cookie:jar, captcha:urlcode?data:captcha});
+      });
+    }else{
+      return callback( {error:"Set cookie error"} );
+    }
+  });
+}
+//main((v)=>console.log(v));
+module.exports = main;
---
+++ test-login.captcha.js
@@ -0,0 +1,4 @@
+var captcha = require("./login.captcha.js");
+
+captcha((v)=>console.log(v), process.argv[2]!=undefined);
+//console.log(captcha)
