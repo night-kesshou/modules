00b9544b45f00f85b918c4127c52d81265ea48d2
ZoneTwelve <zonetwelve159@gmail.com>
2019-01-17 11:48:31 +0800

    新增將一般cookie轉為request cookie

---
+++ generateCookie.js
@@ -0,0 +1,6 @@
+modules.exports = (key, val) => {
+  let cookieJar = request.jar()
+  let cookies = request.cookie(`${key}=${val}`)
+  cookieJar.setCookie(cookies, 'http://210.70.131.56')
+  return cookieJar
+}
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const login = require('./login'),
       captcha = require('./login.captcha'),
-      grade = require('./query.grade');
+      grade = require('./query.grade'),
+      gcj = require('./generateCookie');
 
 exports.login = login;
 exports.captcha = captcha;
 exports.query = {
   grade:grade
 };
+exports.generateCookie = gcj;
