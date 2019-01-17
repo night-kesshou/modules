6e342d66d2af1845590d08bb37b611b7859b8a1d
ZoneTwelve <zonetwelve159@gmail.com>
2019-01-17 19:01:03 +0800

    修改取得的資料型態

--- query.grade.js
+++ query.grade.js
@@ -19,20 +19,15 @@ function main({jar, year}, callback){
       return callback({error:"connect server error", status:r.statusCode});
       let data = (iconv.decode(d, "Big5")); // data translate to big5
       var temp = cheerio.load(data)('table[class="collapse le_06 padding3 spacing0"]>tbody>tr>td');
-      var grade = {0:[], 1:[], total:[], subject:[]};
+      var grade = {subject:[], credit:[], elective:[], score:{0:[], 1:[]}, total:[]};
+      //var grade = {0:[], 1:[], total:[], subject:[]};
       for(var i=11;i<temp.length-2;i+=8){ // 排除非必要資訊, 因此 i從11開始至 html.group(td).length-2 結束
-        grade[0].push({
-          type:temp.eq(i+1).text(),
-          credit:parseInt(temp.eq(i+2).text()),
-          score:parseInt(temp.eq(i+3).text()),
-        });
-        grade[1].push({
-          type:temp.eq(i+4).text(),
-          credit:parseInt(temp.eq(i+5).text()),
-          score:parseInt(temp.eq(i+6).text()),
-        });
-        grade['subject'].push(temp.eq(i).text());
-        grade['total'].push(temp.eq(i+7).text());
+        grade['subject'],push(temp.eq(i).text());
+        grade['credit'].push(parseInt(temp.eq(i+5).text()));
+        grade['elective'].push(temp.eq(i+4).text());
+        grade['score'][0].push(parseInt(temp.eq(i+3).text()));
+        grade['score'][1].push(parseInt(temp.eq(i+6).text()));
+        grade['total'].push(parseInt(temp.eq(i+7).text()));
       }
       return callback({
         data:data,
