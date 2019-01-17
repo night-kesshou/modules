f5c4704c823a44553ca6f4f8f91c495693c8eaa1
ZoneTwelve <zonetwelve159@gmail.com>
2019-01-17 11:53:00 +0800

    由於generateCookie並非爬蟲必須之功能, 因此將其 function 移除

--- generateCookie.js
+++
@@ -1,6 +0,0 @@
-modules.exports = (key, val) => {
-  let cookieJar = request.jar()
-  let cookies = request.cookie(`${key}=${val}`)
-  cookieJar.setCookie(cookies, 'http://210.70.131.56')
-  return cookieJar
-}
--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const login = require('./login'),
       captcha = require('./login.captcha'),
-      grade = require('./query.grade'),
-      gcj = require('./generateCookie');
+      grade = require('./query.grade');
 
 exports.login = login;
 exports.captcha = captcha;
 exports.query = {
   grade:grade
 };
-exports.generateCookie = gcj;
