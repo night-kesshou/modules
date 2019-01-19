f8ae1da9bf573bb5d4d2473b4020348e798b8873
ZoneTwelve <zonetwelve159@gmail.com>
2019-01-20 01:16:35 +0800

    忘記將獎懲請求加入至query

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const login = require('./login'),
       captcha = require('./login.captcha'),
-      grade = require('./query.grade');
+      grade = require('./query.grade'),
+      performance = require('./query.performance');
 
 exports.login = login;
 exports.captcha = captcha;
 exports.query = {
-  grade:grade
+  grade:grade,
+  performance:performance
 };
